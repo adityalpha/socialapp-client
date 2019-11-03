53f2c2ba5e6d381bd95e2b68d41e1163da55f92c
adityalpha <adityalpha@gmail.com>
2019-11-03 16:51:00 +0530

    Adding Post Card Details

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+//import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+//import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 // Components
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 // Pages
-import home from "./pages/home";
-import login from "./pages/login";
-import signup from "./pages/signup";
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#676767",
+      main: "#424242",
+      dark: "#2e2e2e",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#33bfff",
+      main: "#00b0ff",
+      dark: "#007bb2",
+      contrastText: "#000"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
         </div>
-      </Router>
-    </div>
-  );
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Post from "../components/Post";
 
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>home page</h1>
-            </div>
-        )
-    }
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    //Fetching posts
+    axios
+      .get("/posts")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentPostsMarkup = this.state.posts ? (
+      this.state.posts.map(post => <Post post={post} />) //<p>{post.body}</p>)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
